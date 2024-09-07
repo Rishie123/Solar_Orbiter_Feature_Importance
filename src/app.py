818d28fa2e89f185a6ace24ee0ff5f7ebbf0cdaf
Rishabh Jain <88618109+Rishie123@users.noreply.github.com>
2024-09-07 16:54:12 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 import dash
 from dash import dcc, html
 import plotly.express as px
@@ -7,7 +5,6 @@ import pandas as pd
 
 # Load the combined DataFrame
 df = pd.read_csv('combined_feature_importance.csv')
-
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -24,16 +21,41 @@ def prepare_data(df, type_label):
     pivot_df = pivot_df.loc[:, (pivot_df != 0).any(axis=0)]
     return pivot_df
 
-
 # Create Plotly Express graphs for each type
 def create_figure(data, title):
     fig = px.area(data, title=title)
-    fig.update_layout(yaxis_title='Normalized Importance', xaxis_title='Time of Profile')
+    fig.update_layout(
+        title={'text': title, 'font': {'size': 22}},
+        xaxis_title={'text': 'Time of Heater Profile (seconds)', 'font': {'size': 18}},
+        yaxis_title={'text': 'Normalized Importance', 'font': {'size': 18}},
+        legend={'font': {'size': 15}},
+        xaxis={'tickfont': {'size': 14}},
+        yaxis={'tickfont': {'size': 14}}
+    )
     return fig
-
-# Types to include in the dashboard
 types = ['IBS_R', 'IBS_N', 'IBS_T', 'OBS_R', 'OBS_N', 'OBS_T']
-figures = [create_figure(prepare_data(df, type_label), f'Cumulative Normalized Feature Importance of Heater Profiles {type_label}') for type_label in types]
+
+# Define a function to map types to more descriptive titles
+def get_custom_title(type_label):
+    if 'IBS' in type_label:
+        label_type = 'IBS'
+    else:
+        label_type = 'OBS'
+    
+    if '_R' in type_label:
+        direction = 'R direction'
+    elif '_N' in type_label:
+        direction = 'N direction'
+    else:
+        direction = 'T direction'
+    
+    return f'{label_type} along {direction}'
+
+# Create figures with custom titles
+figures = [create_figure(prepare_data(df, type_label), f'Normalized Feature Importance of Heater Profiles for : {get_custom_title(type_label)}') for type_label in types]
+
+# Define the layout of the app to include a graph for each type
+app.layout = html.Div([dcc.Graph(figure=fig) for fig in figures])
 
 # Define the layout of the app to include a graph for each type
 app.layout = html.Div([dcc.Graph(figure=fig) for fig in figures])
